db747b636f5e375f03997d3ab881084abe33a089
Daniel <danielmelo276@gmail.com>
2021-02-20 19:51:15 -0500

    internet check

--- app/src/main/java/com/example/movieapp/repository/MovieRepoImpl.kt
+++ app/src/main/java/com/example/movieapp/repository/MovieRepoImpl.kt
@@ -1,32 +1,43 @@
 package com.example.movieapp.repository
 
+import com.example.movieapp.core.InternetCheck
 import com.example.movieapp.data.local.LocalMovieDataSource
 import com.example.movieapp.data.model.MovieList
 import com.example.movieapp.data.model.toMovieEntity
 import com.example.movieapp.data.resource.MovieDataSourceRemote
 
 class MovieRepoImpl(
-    private val dataSourceRemote: MovieDataSourceRemote,
-    private val dataSourceLocal: LocalMovieDataSource
+        private val dataSourceRemote: MovieDataSourceRemote,
+        private val dataSourceLocal: LocalMovieDataSource
 ) : MovieRepo {
     override suspend fun getUpcomingMovieRepo(): MovieList {
-        dataSourceRemote.getUpcomingMovie().results.forEach { movie ->
-            dataSourceLocal.saveMovie(movie.toMovieEntity("upcoming"))
+        return if (InternetCheck.isNetworkAvailable()) {
+            dataSourceRemote.getUpcomingMovie().results.forEach { movie ->
+                dataSourceLocal.saveMovie(movie.toMovieEntity("upcoming"))
+            }
+            dataSourceLocal.getUpcomingMovies()
+        } else {
+            dataSourceLocal.getUpcomingMovies()
         }
-        return dataSourceLocal.getUpcomingMovies()
     }
 
     override suspend fun getTopRatedMovieRepo(): MovieList {
-        dataSourceRemote.getTopRatedMovie().results.forEach { movie ->
-            dataSourceLocal.saveMovie(movie.toMovieEntity("top_rated"))
-        }
-        return dataSourceLocal.getTopRatedMovies()
+        return if (InternetCheck.isNetworkAvailable()) {
+            dataSourceRemote.getTopRatedMovie().results.forEach { movie ->
+                dataSourceLocal.saveMovie(movie.toMovieEntity("top_rated"))
+            }
+            dataSourceLocal.getTopRatedMovies()
+        } else
+            dataSourceLocal.getTopRatedMovies()
     }
 
     override suspend fun getPopularMoviesRepo(): MovieList {
-        dataSourceRemote.getPopularMovies().results.forEach { movie ->
-            dataSourceLocal.saveMovie(movie.toMovieEntity("popular"))
-        }
-        return dataSourceLocal.getPopularMovies()
+        return if (InternetCheck.isNetworkAvailable()) {
+            dataSourceRemote.getPopularMovies().results.forEach { movie ->
+                dataSourceLocal.saveMovie(movie.toMovieEntity("popular"))
+            }
+            return dataSourceLocal.getPopularMovies()
+        } else
+            dataSourceLocal.getPopularMovies()
     }
 }
